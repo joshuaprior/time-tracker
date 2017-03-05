f9e8410c58f103becb99cbf55031fe776cc4d6fa
Josh Prior <joshuaprior@gmail.com>
2017-03-04 23:49:29 -0800

    Serve the generated front end.

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 
-var app = express();
+var server = express();
 
-app.get('/', (req, res) => {
+server.use(express.static('node_modules/jp-time-tracker/dist'));
+
+server.get('/hello', (req, res) => {
   var time = (new Date()).getTime();
   res.send({msg: 'Hello World!', time: time});
 });
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-app.listen(server_port, server_ip_address, () => console.log('server ready'));
+server.listen(server_port, server_ip_address, () => console.log('server ready'));
