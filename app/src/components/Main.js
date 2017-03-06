39271a37d51e80fb7e2311cd208b94a3330edb9d
Josh Prior <joshuaprior@gmail.com>
2017-03-05 17:04:49 -0800

    wip

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -3,12 +3,14 @@ require('styles/App.css');
 
 import React from 'react';
 import Timer from './timer'
+import Task from './task';
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
         <Timer />
+        <Task />
       </div>
     );
   }
--- app/src/components/timer/timer.js
+++ app/src/components/timer/timer.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import TimeDisplay from './time-display';
+import Tag from '../tag';
+import './style.scss';
 
 class Timer extends React.Component {
   state = {
     startTime: (new Date()).getTime()
   };
 
+  onCurrentTaskClick = () => {
+    console.log('Current task clicked.');
+  }
+
   render() {
+    const currentTask = 'Employee list scroll bar.';
     return (
-      <div>
+      <div className="timer">
+        <Tag label={currentTask} onClick={this.onCurrentTaskClick} />
         <TimeDisplay running={false} startTime={this.state.startTime} />
+        <button>Next</button>
       </div>
     );
   }
